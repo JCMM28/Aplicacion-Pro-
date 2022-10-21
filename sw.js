7e882294f417ec5448372bade9602b3b3527e2ea
jcmmgit2801 <juliocesar.jcmm67@gmail.com>
2022-10-20 19:48:23 -0500

    primer commit

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'v1.cache.awp';
+var urlToCache = [
+    './', 
+    './style.css', 
+    './assets/Earthbound1.jpg',
+    './assets/la-saga-de-earthbound-conocida-.gif',
+    './assets/logo.png',
+    './assets/Mother1.jpg',
+    './assets/mother3.jpg',
+    './assets/onettnight.png',
+    './assets/Earthbound.tff',
+]
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlToCache)
+            .then(() => {
+                self.skipWaiting();
+            })
+        })
+        .catch(err => {
+            console.log("NO SE HA RESGUARDADO EL CACHE", err)
+        })
+    )
+})
+
+// event activate
+self.addEventListener('activate', e => {
+    const cacheWhiteList = [CACHE_NAME];
+    e.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if(cacheWhiteList.indexOf(cacheName) === - 1){
+                        // borrar los elementos que no necesita
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+        .then(() => {
+            // activar la cache
+            self.clients.claim();
+        })
+    )
+})
+
+// evento fetch
+self.addEventListener('fetch', e =>{
+    e.respondWith(
+        caches.match(e.request)
+        .then(res => {
+            if(res){
+                return res;
+            }
+            return fetch(e.request);
+        })
+    )
+})
